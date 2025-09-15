41a0e490f151b5199ff0fd2d21875b2c4e1c31b6
Malakai <poncemalakai@gmail.com>
2025-09-14 18:05:09 -0700

    Commit 2 - this function uses a for loop to take in the users data

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+const int N = 5; // Size of the array
+
+void enterArrayData(double* a, int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << " Enter element " << i << ": "; //  Whole block asks for user input
+        cin >> *(a + i);                        //  and stores it in the array
+    }
+    cout << "Data entered :)" << endl;
+}
+
